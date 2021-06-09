9b25b3ff6d9782782390f00178794b9130cd7b31
Tommi Rantanen <tommi.rantanen@nordicsemi.no>
2021-06-09 16:11:00 +0300

    mosh: DO NOT MERGE TO NCS. Enable FIDO tracing.
    Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

--- samples/nrf9160/modem_shell/src/main.c
+++ samples/nrf9160/modem_shell/src/main.c
@@ -208,12 +208,62 @@ static void button_handler(uint32_t button_states, uint32_t has_changed)
 }
 #endif
 
+/***** START OF INTERNAL CHANGE. DO NOT COMMIT TO NCS MASTER. *****/
+
+#if !defined(CONFIG_BOARD_THINGY91_NRF9160_NS)
+#include <hal/nrf_gpio.h>
+static void modem_trace_enable(void)
+{
+/* GPIO configurations for trace and debug */
+#define CS_PIN_CFG_TRACE_CLK 21 //GPIO_OUT_PIN21_Pos
+#define CS_PIN_CFG_TRACE_DATA0 22 //GPIO_OUT_PIN22_Pos
+#define CS_PIN_CFG_TRACE_DATA1 23 //GPIO_OUT_PIN23_Pos
+#define CS_PIN_CFG_TRACE_DATA2 24 //GPIO_OUT_PIN24_Pos
+#define CS_PIN_CFG_TRACE_DATA3 25 //GPIO_OUT_PIN25_Pos
+
+	// Configure outputs.
+	// CS_PIN_CFG_TRACE_CLK
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_CLK] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA0
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA0] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA1
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA1] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA2
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA2] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA3
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA3] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	NRF_P0_NS->DIR = 0xFFFFFFFF;
+}
+#endif
+/***** END OF INTERNAL CHANGE *****/
+
 void main(void)
 {
 	int err;
 	struct k_work_queue_config cfg = {
 		.name = "mosh_common_workq",
 	};
+
+	/***** START OF INTERNAL CHANGE */
+#if !defined(CONFIG_BOARD_THINGY91_NRF9160_NS)
+	modem_trace_enable();
+#endif
+	/***** END OF INTERNAL CHANGE *****/
 
 #if defined(CONFIG_SHELL_BACKEND_SERIAL)
 	mosh_shell = shell_backend_uart_get_ptr();
