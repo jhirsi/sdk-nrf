5e8c413e1ee91322768174a73c3043dfdfa562c8
Tommi Rantanen <tommi.rantanen@nordicsemi.no>
2021-06-09 16:11:00 +0300

    mosh: DO NOT MERGE TO NCS. Enable FIDO tracing.
    Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

--- samples/nrf9160/modem_shell/src/main.c
+++ samples/nrf9160/modem_shell/src/main.c
@@ -106,10 +106,57 @@ static void button_handler(uint32_t button_states, uint32_t has_changed)
 	}
 }
 
+/***** START OF INTERNAL CHANGE. DO NOT COMMIT TO NCS MASTER. *****/
+
+#include <hal/nrf_gpio.h>
+static void modem_trace_enable(void)
+{
+/* GPIO configurations for trace and debug */
+#define CS_PIN_CFG_TRACE_CLK 21 //GPIO_OUT_PIN21_Pos
+#define CS_PIN_CFG_TRACE_DATA0 22 //GPIO_OUT_PIN22_Pos
+#define CS_PIN_CFG_TRACE_DATA1 23 //GPIO_OUT_PIN23_Pos
+#define CS_PIN_CFG_TRACE_DATA2 24 //GPIO_OUT_PIN24_Pos
+#define CS_PIN_CFG_TRACE_DATA3 25 //GPIO_OUT_PIN25_Pos
+
+	// Configure outputs.
+	// CS_PIN_CFG_TRACE_CLK
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_CLK] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA0
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA0] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA1
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA1] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA2
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA2] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA3
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA3] =
+		(GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	NRF_P0_NS->DIR = 0xFFFFFFFF;
+}
+
+/***** END OF INTERNAL CHANGE *****/
+
 void main(void)
 {
 	int err;
 	const struct shell *shell = shell_backend_uart_get_ptr();
+
+	/***** START OF INTERNAL CHANGE */
+	modem_trace_enable();
+	/***** END OF INTERNAL CHANGE *****/
 
 	mosh_print_version_info();
 
