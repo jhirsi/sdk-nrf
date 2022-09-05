3bc3d70076a570e93bfcfce902ed76614cb459d0
Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>
2022-09-05 13:56:01 +0300

    lib: location: support for nrf wifi management if nrf700x
    Signed-off-by: Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>

--- lib/location/method_wifi.c
+++ lib/location/method_wifi.c
@@ -6,12 +6,6 @@
 
 #include <stdio.h>
 
-#if defined(CONFIG_WIFI_NRF700X)
-#include <stdlib.h>
-#include <ctype.h>
-#include <zephyr/init.h>
-#endif
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
@@ -19,9 +13,6 @@
 #include <zephyr/net/net_event.h>
 #include <zephyr/net/wifi_mgmt.h>
 
-#if defined(CONFIG_WIFI_NRF700X)
-#include <zephyr_fmac_main.h>
-#endif
 #include <modem/location.h>
 #include "location_core.h"
 #include "location_utils.h"
@@ -48,8 +39,6 @@ static struct method_wifi_start_work_args method_wifi_start_work;
 static bool running;
 
 static uint32_t current_scan_result_count;
-static uint32_t current_scan_result_with_mac_count;
-
 static uint32_t latest_scan_result_count;
 
 struct method_wifi_scan_result {
@@ -64,77 +53,6 @@ static struct method_wifi_scan_result
 static K_SEM_DEFINE(wifi_scanning_ready, 0, 1);
 
 /******************************************************************************/
-
-#if defined(CONFIG_WIFI_NRF700X)
-
-static void scan_result_cb(struct net_if *iface,
-			   int status,
-			   struct wifi_driver_scan_result *entry)
-{
-	if (!iface) {
-		return;
-	}
-
-	if (!entry) {
-		if (status) {
-			printk("Scan request failed (%d)\n", status);
-		} else {
-			printk("Scan request done\n");
-			latest_scan_result_count =
-				(current_scan_result_with_mac_count >
-				 CONFIG_LOCATION_METHOD_WIFI_SCANNING_RESULTS_MAX_CNT) ?
-					CONFIG_LOCATION_METHOD_WIFI_SCANNING_RESULTS_MAX_CNT :
-					current_scan_result_with_mac_count;
-			current_scan_result_count = 0;
-			current_scan_result_with_mac_count = 0;
-			k_sem_give(&wifi_scanning_ready);
-		}
-		return;
-	}
-
-	current_scan_result_count++;
-
-	if (current_scan_result_count == 1U) {
-		printk("\n%-4s | %-32s %-5s | %-4s | %-4s | %-8s | %-12s\n", "Num", "SSID",
-			"(len)", "Chan", "RSSI", "Sec", "BSSID");
-	}
-	struct method_wifi_scan_result *current;
-
-	printk(
-		"%-4d | %-32s %-5u | %-4u | %-4d | %-8s | "
-		"%02x:%02x:%02x:%02x:%02x:%02x\n",
-		      current_scan_result_count,
-		      entry->ssid,
-		      entry->ssid_length,
-		      entry->channel,
-		      entry->rssi,
-		      (entry->security == WIFI_SECURITY_TYPE_PSK ? "WPA/WPA2" : "Open"),
-		      entry->mac[0], entry->mac[1],
-		entry->mac[2], entry->mac[3],
-		entry->mac[4], entry->mac[5]);
-
-	if (1) { //TODO: should be? if (entry->mac_length)
-		current_scan_result_with_mac_count++;
-
-		if (current_scan_result_with_mac_count <=
-		    CONFIG_LOCATION_METHOD_WIFI_SCANNING_RESULTS_MAX_CNT) {
-			current = &latest_scan_results[current_scan_result_with_mac_count - 1];
-
-			sprintf(current->mac_addr_str, "%02x:%02x:%02x:%02x:%02x:%02x",
-				entry->mac[0], entry->mac[1], entry->mac[2], entry->mac[3],
-				entry->mac[4], entry->mac[5]);
-			snprintf(current->ssid_str, entry->ssid_length + 1, "%s", entry->ssid);
-
-			current->channel = entry->channel;
-			current->rssi = entry->rssi;
-
-			LOG_DBG("scan result #%d stored: ssid %s, mac address: %s, channel %d,",
-				current_scan_result_with_mac_count, current->ssid_str,
-				current->mac_addr_str, current->channel);
-		}
-	}
-}
-#else
 
 static void method_wifi_scan_result_handle(struct net_mgmt_event_callback *cb)
 {
@@ -205,7 +123,6 @@ void method_wifi_net_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint
 		}
 	}
 }
-#endif
 
 /******************************************************************************/
 
@@ -219,18 +136,12 @@ static int method_wifi_scanning_start(void)
 	current_scan_result_count = 0;
 
 	__ASSERT_NO_MSG(wifi_iface != NULL);
-#if defined(CONFIG_WIFI_NRF700X)
-	const struct wifi_nrf_dev_ops *dev_ops = wifi_dev->api;
-
-	return dev_ops->off_api.disp_scan(wifi_dev,
-					  scan_result_cb);
-#else
 	ret = net_mgmt(NET_REQUEST_WIFI_SCAN, wifi_iface, NULL, 0);
 	if (ret) {
 		LOG_ERR("Failed to initiate Wi-Fi scanning: %d", ret);
 		ret = -EFAULT;
 	}
-#endif
+
 	return ret;
 }
 
@@ -375,10 +286,9 @@ int method_wifi_init(void)
 	wifi_iface = NULL;
 
 #if defined(CONFIG_WIFI_NRF700X)
-	wifi_iface = net_if_get_default(); /* TODO: not default always */
-	wifi_dev = net_if_get_device(wifi_iface);
-	if (wifi_iface == NULL) {
-		LOG_ERR("Could not get the Wi-Fi net interface");
+	wifi_dev = device_get_binding("wlan0"); /* TODO */
+	if (wifi_dev == NULL) {
+		LOG_ERR("Could not get the Wi-Fi dev");
 		return -EFAULT;
 	}
 #if defined(CONFIG_LOCATION_METHOD_WIFI_SERVICE_HERE)
@@ -425,12 +335,10 @@ static const char tls_certificate[] =
 		LOG_WRN("tls_credential_add() was a success!!!\n");
 	}
 #endif
-
-
 #else
 	wifi_dev = DEVICE_DT_GET(DT_CHOSEN(ncs_location_wifi));
+#endif
 
-	wifi_iface = NULL;
 	if (!device_is_ready(wifi_dev)) {
 		LOG_ERR("Wi-Fi device %s not ready", wifi_dev->name);
 		return -ENODEV;
@@ -445,6 +353,5 @@ static const char tls_certificate[] =
 	net_mgmt_init_event_callback(&method_wifi_net_mgmt_cb, method_wifi_net_mgmt_event_handler,
 				     (NET_EVENT_WIFI_SCAN_RESULT | NET_EVENT_WIFI_SCAN_DONE));
 	net_mgmt_add_event_callback(&method_wifi_net_mgmt_cb);
-#endif
 	return 0;
 }
