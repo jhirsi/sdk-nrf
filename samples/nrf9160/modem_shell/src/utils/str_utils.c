fe1e9b7026a4b499fc0deb6d3eb0ae83073fa4cb
Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>
2022-11-23 16:47:50 +0200

    samples: nrf9160: modem_shell: common util for getting shell cmd str
    Common util from plain th usage to common mosh utils.
    That can be used to get a shell command string from argv[].
    At the same time, trimming off ending space.
    
    Signed-off-by: Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>

--- samples/nrf9160/modem_shell/src/utils/str_utils.c
+++ samples/nrf9160/modem_shell/src/utils/str_utils.c
@@ -15,6 +15,28 @@
 
 #include "str_utils.h"
 
+char *get_command_str_from_argv(size_t argc, char **argv, char *out_buf, uint16_t out_buf_len)
+{
+	int i, total_len = 0, arg_len = 0;
+
+	for (i = 0; i < argc; i++) {
+		arg_len = strlen(argv[i]);
+		if (total_len + arg_len > out_buf_len) {
+			break;
+		}
+		if (i == (argc - 1)) {
+			/* Do not put space to last one */
+			sprintf(out_buf + total_len, "%s", argv[i]);
+
+		} else {
+			sprintf(out_buf + total_len, "%s ", argv[i]);
+		}
+		total_len = strlen(out_buf);
+	}
+
+	return out_buf;
+}
+
 int str_hex_to_bytes(char *str, uint32_t str_length, uint8_t *buf, uint16_t buf_length)
 {
 	/* Remove any spaces from the input string */
--- samples/nrf9160/modem_shell/src/utils/str_utils.h
+++ samples/nrf9160/modem_shell/src/utils/str_utils.h
@@ -7,6 +7,7 @@
 #ifndef MOSH_STR_UTILS_H
 #define MOSH_STR_UTILS_H
 
+char *get_command_str_from_argv(size_t argc, char **argv, char *out_buf, uint16_t out_buf_len);
 int str_hex_to_bytes(char *str, uint32_t str_length, uint8_t *buf, uint16_t buf_length);
 char *mosh_strdup(const char *str);
 
