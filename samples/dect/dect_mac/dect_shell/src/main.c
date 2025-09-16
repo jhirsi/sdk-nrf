8804bdfa9726029c86aafdb68f93ad0865ab0bce
Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>
2025-09-16 11:03:40 +0300

    samples: dect_mac: dect_shell: fido enabled (DO NOT MERGE, nor make public!!!!!!)
    As a default fido is enabled (except for Thingy), to disable use Kconfig option e.g.:
    west build -p -b nrf9151dk/nrf9151/ns -- -DCONFIG_DESH_FIDO_TRACE=n
    
    Signed-off-by: Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>

--- samples/dect/dect_mac/dect_shell/src/main.c
+++ samples/dect/dect_mac/dect_shell/src/main.c
@@ -172,6 +172,57 @@ static void button_handler(uint32_t button_states, uint32_t has_changed)
 }
 #endif
 
+/***** START OF INTERNAL CHANGE. DO NOT COMMIT TO NCS MASTER. *****/
+#if defined(CONFIG_DESH_FIDO_TRACE) && !defined(CONFIG_BOARD_THINGY91_NRF9160_NS)
+#include <zephyr/init.h>
+#include <nrf.h>
+#include <hal/nrf_gpio.h>
+
+static int modem_trace_enable(void)
+{
+	/* GPIO configurations for trace and debug */
+	#define CS_PIN_CFG_TRACE_CLK	21 //GPIO_OUT_PIN21_Pos
+	#define CS_PIN_CFG_TRACE_DATA0	22 //GPIO_OUT_PIN22_Pos
+	#define CS_PIN_CFG_TRACE_DATA1	23 //GPIO_OUT_PIN23_Pos
+	#define CS_PIN_CFG_TRACE_DATA2	24 //GPIO_OUT_PIN24_Pos
+	#define CS_PIN_CFG_TRACE_DATA3	25 //GPIO_OUT_PIN25_Pos
+
+	// Configure outputs.
+	// CS_PIN_CFG_TRACE_CLK
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_CLK] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA0
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA0] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA1
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA1] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA2
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA2] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	// CS_PIN_CFG_TRACE_DATA3
+	NRF_P0_NS->PIN_CNF[CS_PIN_CFG_TRACE_DATA3] = (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos) |
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos);
+
+	NRF_P0_NS->DIR = 0xFFFFFFFF;
+#if RM_JH /* alternative way */
+	*(uint32_t *)0x40842754 = 0x302;
+	*(uint32_t *)0x40842758 = 0x302;
+	*(uint32_t *)0x4084275c = 0x302;
+	*(uint32_t *)0x40842760 = 0x302;
+	*(uint32_t *)0x40842764 = 0x302;
+#endif
+	return 0;
+}
+
+SYS_INIT(modem_trace_enable, PRE_KERNEL_1, 0);
+#endif
+/***** END OF INTERNAL CHANGE *****/
+
 int main(void)
 {
 	int err;
